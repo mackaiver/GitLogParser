9bf0792104c8cd930dafcc93181630ceea4a0411
Kai B <kai.bruegge@tu-dortmund.de>
2016-08-25 19:17:15 +0200

    add date and diff informaiton to output

--- gitLogParser.py
+++ gitLogParser.py
@@ -5,44 +5,73 @@ import re
 
 # array to store dict of commit data
 commits = []
+files_changed_pattern = re.compile(' (\\d+) files* changed')
+insertion_pattern = re.compile('\\s(\\d+) insertions*')
+deletion_pattern = re.compile('\\s(\\d+) deletions*')
 
 def parseCommit(commitLines):
 	# dict to store commit data
 	commit = {}
+
 	# iterate lines and save
-	for nextLine in commitLines:
-		if nextLine == '' or nextLine == '\n':
+	for commit_line in commitLines:
+		# print(commit_line)
+		if commit_line == '' or commit_line == '\n':
 			# ignore empty lines
 			pass
-		elif bool(re.match('commit', nextLine, re.IGNORECASE)):
+		elif bool(re.match('commit', commit_line, re.IGNORECASE)):
 			# commit xxxx
-			if len(commit) != 0:		## new commit, so re-initialize
-				commits.append(commit)
-				commit = {}
-			commit = {'hash' : re.match('commit (.*)', nextLine, re.IGNORECASE).group(1) }
-		elif bool(re.match('merge:', nextLine, re.IGNORECASE)):
+			# if len(commit) != 0:		## new commit, so re-initialize
+			commit = {'hash' : re.match('commit (.*)', commit_line, re.IGNORECASE).group(1) }
+			commit['files_changed'] = 0
+			commit['insertions'] = 0
+			commit['deletions'] = 0
+			commits.append(commit)
+
+		elif bool(re.match('merge:', commit_line, re.IGNORECASE)):
 			# Merge: xxxx xxxx
 			pass
-		elif bool(re.match('author:', nextLine, re.IGNORECASE)):
+		elif bool(re.match('author:', commit_line, re.IGNORECASE)):
 			# Author: xxxx <xxxx@xxxx.com>
-			m = re.compile('Author: (.*) <(.*)>').match(nextLine)
+			m = re.compile('Author: (.*) <(.*)>').match(commit_line)
 			commit['author'] = m.group(1)
 			commit['email'] = m.group(2)
-		elif bool(re.match('date:', nextLine, re.IGNORECASE)):
-			# Date: xxx
+		elif bool(re.match('date:', commit_line, re.IGNORECASE)):
+			m = re.match('Date:\s+(.+)', commit_line)
+			if m:
+				commit['date'] = m.group(1)
 			pass
-		elif bool(re.match('    ', nextLine, re.IGNORECASE)):
+		elif bool(re.match('    ', commit_line, re.IGNORECASE)):
 			# (4 empty spaces)
 			if commit.get('message') is None:
-				commit['message'] = nextLine.strip()
+				commit['message'] = commit_line.strip().replace(';', ',')
 		else:
-			print ('ERROR: Unexpected Line: ' + nextLine)
+			if files_changed_pattern.search(commit_line):
+				m = files_changed_pattern.search(commit_line)
+				if m:
+					commit['files_changed'] = m.group(1)
+
+			if insertion_pattern.search(commit_line):
+				m = insertion_pattern.search(commit_line)
+				if m:
+					commit['insertions'] = m.group(1)
+
+			if deletion_pattern.search(commit_line):
+				m = deletion_pattern.search(commit_line)
+				if m:
+					commit['deletions'] = m.group(1)
+
 
 if __name__ == '__main__':
 	parseCommit(sys.stdin.readlines())
-
-	# print commits
-	print 'Author'.ljust(15) + '  ' + 'Email'.ljust(20) +'  ' + 'Hash'.ljust(8) + '  ' + 'Message'.ljust(20)
-	print "================================================================================="
+	print('author;email;date;hash;message;files_changed;insertions;deletions')
 	for commit in commits:
-		print commit['author'].ljust(15) + '  ' + commit['email'][:20].ljust(20) + '  ' +  commit['hash'][:7].ljust(8) + '  ' + commit['message']
+		print('{};{};{};{};{};{};{};{}'
+					.format(commit['author'],
+							commit['email'],
+							commit['date'],
+							commit['hash'],
+							commit['message'],
+							commit['files_changed'],
+							commit['insertions'],
+							commit['deletions']))
